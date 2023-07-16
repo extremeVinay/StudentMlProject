da9ca027885e6b429bb43c59fe77f7ed1007f320
Vinay <learningfrombirth@gmail.com>
2023-07-16 17:24:21 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='StudentMlProject',
+name='mlproject',
 version='0.0.1',
 author='Vinay',
 author_email='learningfrombirth@gmail.com',
